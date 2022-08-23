868388bbddfce5e92e2947d316116b9a23274199
parkhom.nata <natalia.parkhomets@gmail.com>
2022-08-23 23:22:54 +0300

    Added date and current weather for a particular location

---
+++ src/app.js
@@ -0,0 +1,31 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    } 
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+
+function displayTemperature(response){
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#current-temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city").innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description").innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "2b5667a8237d1b01430707e2a1deb6dc";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
